a6c67b2c6a3b387adbb1e30f10eb8e40f8d316e5
ArthurPro123 <arthur_ssl@hotmail.com>
2024-11-30 09:22:43 +0700

    edited test_models.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -168,13 +168,14 @@ class TestProductRoutes(TestCase):
     #
 
     def test_get_product(self):
-        """It should Read a single Product"""
-        products = self._create_products(1)
-        test_product = products[0]
+        """It should Get a single Product"""
+        # get the id of a product
+        test_product = self._create_products(1)
+
         response = self.client.get(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
-        self.assertEqual(data["id"], test_product.name)
+        self.assertEqual(data["name"], test_product.name)
 
     ######################################################################
     # Utility functions
