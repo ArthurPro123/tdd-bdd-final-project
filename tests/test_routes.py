a1621bcfeda836826b5afdc37570fc0b642e1aaf
ArthurPro123 <arthur_ssl@hotmail.com>
2024-11-30 09:24:01 +0700

    edited test_models.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -170,7 +170,7 @@ class TestProductRoutes(TestCase):
     def test_get_product(self):
         """It should Get a single Product"""
         # get the id of a product
-        test_product = self._create_products(1)
+        test_product = self._create_products(1)[0]
 
         response = self.client.get(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
